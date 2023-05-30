e9f20be7384d98ac27a936044d9765e14dff3b85
pablo <pablodjeufoue@gmail.com>
2023-05-30 23:39:06 +0100

    Add Index CityOrApartement

---
+++ Logement/Migrations/20230530223755_AddIndexCityOrApartement.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logement.Migrations
+{
+    public partial class AddIndexCityOrApartement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CityOrApartement",
+                table: "Fichiers",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fichiers_CityOrApartement",
+                table: "Fichiers",
+                column: "CityOrApartement");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Fichiers_CityOrApartement",
+                table: "Fichiers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityOrApartement",
+                table: "Fichiers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
