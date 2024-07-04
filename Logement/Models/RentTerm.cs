deb525088475a147589e230af6aa017ddc69317c
pablo <pablodjeufoue@gmail.com>
2024-07-04 16:58:28 +0100

    Update

---
+++ Logement/Models/RentTerm.cs
@@ -0,0 +1,19 @@
+﻿using Logement.Data.Enum;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logement.Models
+{
+    public class RentTerm
+    {
+        public int Id { get; set; }
+        public int TenancyId { get; set; }
+        public virtual Tenancy? Tenancy { get; set; }
+        public RentStatusEnum RentStatus { get; set; }
+
+        [Precision(14, 2)]
+        public decimal Price { get; set; }
+        public bool IsFirstPaymemt { get; set; }
+        public bool IsRentPayForThisMonth { get; set; }
+        public decimal DepositPrice {get;set;}
+    }
+}
